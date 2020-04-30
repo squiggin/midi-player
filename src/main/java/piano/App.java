081280fcd0c11947eaf50bfcd883509b078e6993
Pratham Purohit <prathamjp02@gmail.com>
2020-04-30 16:27:17 +1000

    Initial Commit

---
+++ src/main/java/piano/App.java
@@ -0,0 +1,29 @@
+package piano;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+    public App() {
+        // Initialise variables here
+    }
+
+    public void settings() {
+        // Don't touch
+        size(540, 335);
+    }
+
+    public void setup() {
+        frameRate(60);
+        // Load images here
+    }
+
+    public void draw() {
+        // Draw your program here
+    }
+
+    public static void main(String[] args) {
+        // Don't touch this
+        PApplet.main("piano.App");
+    }
+}
